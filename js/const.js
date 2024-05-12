b10b2a152e6e87c70bcb6e5f640bd2ddb447720a
takillme <145220482+takillme@users.noreply.github.com>
2024-05-13 01:26:57 +0900

    Update const.js

--- js/const.js
+++ js/const.js
@@ -1,1 +1,5 @@
-WSURL = 'ws://localhost:5000';
+// WSURL = 'wss://catasorasp.omatz.cf';
+// WSURL = 'wss://cataso2.omatz.cf';
+// WSURL = 'wss://cataso-omatz.herokuapp.com';
+// WSURL = 'ws://localhost:5000';
+WSURL = 'wss://cataso-server.onrender.com';
